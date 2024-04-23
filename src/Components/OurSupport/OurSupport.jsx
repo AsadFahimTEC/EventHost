da329ab6cdf5a77af172eeee6ee3832b223137bd
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-04-24 00:13:02 +0600

    host your event section finish properly

--- src/Components/OurSupport/OurSupport.jsx
+++ src/Components/OurSupport/OurSupport.jsx
@@ -1,55 +1,49 @@
-import { useState } from "react";
-import toast from "react-hot-toast";
-
 const OurSupport = () => {
-  const [email, setEmail] = useState("");
-
-  const handleInputChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleSubscribe = () => {
-    // Validate email format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailRegex.test(email)) {
-      toast.success("Thank you for subscribing!");
-    } else {
-      toast.error("Invalid email. Please enter a valid email address.");
-    }
-  };
-
   return (
-    <div className="mt-0 bg-violet-800">
-      <div className="py-6 text-white">
-        <div className="container mx-auto flex flex-col justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:space-x-12 lg:justify-between lg:flex-row">
-          <div className="flex flex-col space-y-4 text-center lg:text-left">
-            <h1 className="text-3xl font-bold leading">
-              <span className="text-[#FF9F0D]">S</span>till You Need Our
-              Support?
-            </h1>
-            <p className="text-white">
-              Don’t wait, make a smart & logical quote here. It's pretty easy.
-            </p>
-          </div>
-          <div className="flex flex-row items-center self-center rounded-l-lg justify-center flex-shrink-0 shadow-lg text-white lg:justify-end">
-            <div className="flex flex-row">
-              <input
-                type="text"
-                placeholder="Enter Your Email"
-                value={email}
-                onChange={handleInputChange}
-                className="w-3/5 placeholder-white p-2 rounded-l-lg sm:w-2/3 text-white bg-[#FF9F0D]"
-              />
-              <button
-                type="button"
-                onClick={handleSubscribe}
-                className="w-2/5 p-2 font-semibold rounded-r-lg sm:w-1/3 dark:bg-white text-[#FF9F0D]"
-              >
-                Subscribe
-              </button>
-            </div>
-          </div>
-        </div>
+    <div className="mt-4 mb-72">
+      <img
+        className="mx-auto ml-32 w-[1000px] h-[424px] rounded-lg"
+        src="https://i.ibb.co/6ZYnCK5/navigation.png"
+        alt=""
+      />
+      <div>
+        <img
+          className="mx-auto -mt-96 ml-48"
+          src="https://i.ibb.co/1YCXgHz/Group-29.png"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          className="mx-auto -mt-56 mr-56"
+          src="https://i.ibb.co/hyq9LGL/Group.png"
+          alt=""
+        />
+      </div>
+      <div>
+        <h1 className="text-2xl text-white text-center -mt-80 -mr-36 ml-36">
+          Host Your Event Easily and Automatedly <br /> With Us
+        </h1>
+      </div>
+      <div>
+        <h1 className="text-xl text-center ml-52 text-white mt-4">
+          We Provide The Best Event Ticketing <br />
+          and Management Solutions.
+        </h1>
+      </div>
+      <div>
+        <li className="text-sm text-center text-white ml-52 mt-2">
+          Host Your Events With Easy Steps!
+        </li>
+        <li className="text-sm text-center text-white ml-52 mt-2">
+          Automated Payment System Pro.
+        </li>
+        <li className="text-sm text-center text-white ml-52 mt-2">
+          Access To Registration Time Rep.
+        </li>
+        <li className="text-sm text-center text-white ml-52 mt-2">
+          Send Bulk Mail Easily With Atta
+        </li>
       </div>
     </div>
   );
