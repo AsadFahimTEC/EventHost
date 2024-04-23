f2f04ca52618cfa260a69417fdbf70f52aa9c149
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-04-24 04:46:55 +0600

    ticketing section design finished properly

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,13 +1,13 @@
 const Gallery = () => {
   return (
     <div className="mt-4 mb-2">
-      <h1 className="text-center text-3xl text-[#111111]">
+      <h1 className="text-center text-3xl text-[#111111] font-bold">
         Automated QR Scanner
       </h1>
       <h6 className="text-center text-md text-violet-600 mt-2">
         Scan & Verify The Attendees Directly By Our QR Scanning <br /> Solution.
       </h6>
-      <div className="flex items-center justify-center gap-4 mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-4 ml-28 mr-28">
         <div className="bg-[#F1F3FF] card">
           <img
             className="w-6 h-6"
